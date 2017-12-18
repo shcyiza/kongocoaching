87450e176a8859343ab0bdcc45219d5c07a3ef14
shcyiza <patick.cyiza@gmail.com>
2017-12-18 07:28:58 +0100

    api show page finished

--- app/controllers/api/v1/storefront_api_responder_controller.rb
+++ app/controllers/api/v1/storefront_api_responder_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::StorefrontApiResponderController < Api::V1::ApiBaseController
     private_key = request.headers["HTTP_SECRET_KEY"]
     @authorize_crew_id = CrewStorefrontKey.check_credentials_and_return_crew public_key, private_key
     if @authorize_crew_id == false
-      respond_with_error(401, 'Bad credentials')
+      respond_with_error(401, 'Not authorized')
     else
       @crew =  Crew.find(@authorize_crew_id)
     end
